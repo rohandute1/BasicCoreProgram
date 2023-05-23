3ab3371700e841e80cf7a9044e9df55c8f3d35fd
Rohan <rohandute1@gmail.com>
2023-05-23 14:58:01 +0530

    [Rohan] Factorization

---
+++ BasicCoreProgram/PrimeFactorization.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class PrimeFactorization
+    {
+        public static void FindPrimeFactors(int N) 
+        {
+            if (N<= 1) 
+            {
+                Console.WriteLine("Invalid input. N should be a positive integer greater than 1.");
+                return;
+            }
+            Console.WriteLine("Prime factors of " + N + ": ");
+
+            for (int i = 2; i * i <= N; i++) 
+            {
+                while (N % i == 0) 
+                {
+                    Console.Write(i + " ");
+                    N /= i;
+                }
+            }
+
+            if (N > 1) 
+            {
+                Console.Write(N); 
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -11,32 +11,52 @@ namespace BasicCoreProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Programs");
-
-            CoinFlipper flipper = new CoinFlipper();
-            CoinFlipper.FlipCoin(100);
-
-            LeapYear checker = new LeapYear();
-            int year = 2016; //Example: Year to check
-            if (LeapYear.IsLeapYear(year)) 
+            Console.WriteLine("Please choose any one program.");
+            Console.WriteLine(" 1.Coin flipper\n 2.Leap year\n 3.Power of 2\n 4.Harmonic numbers\n 5.Prime factorization");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
             {
-                Console.WriteLine(year + " is a leap year.");
-            }
-            else 
-            {
-                Console.WriteLine(year + " is not a leap year.");
-            }
+                case 1:
+                    CoinFlipper flipper = new CoinFlipper();
+                    CoinFlipper.FlipCoin(100);
+                    break;
+                case 2:
+                    Console.WriteLine("Please enter the year:");
+                    LeapYear checker = new LeapYear();
+                    int year = Convert.ToInt32(Console.ReadLine()); //Example: Year to check
+                    if (LeapYear.IsLeapYear(year))
+                    {
+                        Console.WriteLine(year + " is a leap year.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(year + " is not a leap year.");
+                    }
+                    break;
 
-            PowerOf2 table = new PowerOf2();
-            int N = 5;
-            PowerOf2.PrintPowerOfTwoTable(N);
-             
-            HarmonicNumbers harmonic = new HarmonicNumbers();
-            int X = 10;
-            double result = HarmonicNumbers.CalculateHarmonicNumber(X);
-            Console.WriteLine("Nth Harmonic Value: " + result);
+                case 3:
+                    Console.WriteLine("Please enter the number:");
+                    PowerOf2 table = new PowerOf2();
+                    int N = Convert.ToInt32(Console.ReadLine());
+                    PowerOf2.PrintPowerOfTwoTable(N);
+                    break;
+
+                case 4:
+                    Console.WriteLine("Please enter the number:");
+                    HarmonicNumbers harmonic = new HarmonicNumbers();
+                    int X = Convert.ToInt32(Console.ReadLine());
+                    double result = HarmonicNumbers.CalculateHarmonicNumber(X);
+                    Console.WriteLine("Nth Harmonic Value: " + result);
+                    break;
+
+                case 5:
+                    Console.WriteLine("Please enter the number:");
+                    PrimeFactorization factorization = new PrimeFactorization();
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    PrimeFactorization.FindPrimeFactors(x);
+                    break;
+            }
             Console.ReadLine();
-
-
-        }
+        } 
     }
 }
